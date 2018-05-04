a73cd6c27b6e9811a568ec6f55fbf886c23c3b34
songjiachao <songjiachao@weshare.com.cn>
2018-05-04 22:22:28 +0800

    添加新闻列表请求

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,26 @@
+const config = require('../../config/config');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    news: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-
+    this.getListOfNews();
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getListOfNews: function () {
+    wx.request({
+      url: config.url + '/list',
+      method: 'POST',
+      data: {
+        size: '5',
+        type: 'news'
+      },
+      success: function (res) {
+        console.log(res.data.data)
+      }
+    })
   }
 })
