e9f989fde658464b345c81e7e94fab254aed3ac3
songjiachao <songjiachao@weshare.com.cn>
2018-05-06 12:03:40 +0800

    添加理财产品

--- pages/finance/finance.js
+++ pages/finance/finance.js
@@ -1,65 +1,84 @@
+const config = require('../../config/config');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    index: 0,
+    sortType: ["默认排序", "收益降序", "收益升序"]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    this.getProductList();
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
+  },
+  getProductList: function () {
+    wx.request({
+      url: config.url + '/getProductList',
+      success: (res) => {
+        this.setData({
+          product: {
+            data: res.data.data
+          }
+        });
+      }
+    })
+  },
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value
+    })
   }
 })
