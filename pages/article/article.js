48ad7b34b4311d106527917056cfddac20e56e36
songjiachao <songjiachao@weshare.com.cn>
2018-05-08 16:21:23 +0800

    添加文章页面

---
+++ pages/article/article.js
@@ -0,0 +1,78 @@
+const config = require('../../config/config');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options.id);
+    this.getArticleDetails(options.id);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  getArticleDetails: function (id) {
+    wx.request({
+      url: config.url + '/getArticleDetails',
+      method: "POST",
+      data: {
+        id: id
+      },
+      success: (res) => {
+        this.setData({ ...res.data.data });
+      }
+    })
+  }
+})
